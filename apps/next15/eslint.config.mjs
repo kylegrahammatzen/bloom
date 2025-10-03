69a9ad700d54904b4919f7f94e5c43a657a63274
Kyle <kgrahammatzen@gmail.com>
2025-10-03 16:18:20 -0400

    chore(frontend): add ESLint configuration and dependencies

---
+++ apps/next15/eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+const eslintConfig = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
+
+export default eslintConfig;
