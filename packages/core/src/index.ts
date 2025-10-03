86269b71587d7007a33c4d0efdf993d84a4ff1cf
Kyle <kgrahammatzen@gmail.com>
2025-10-03 10:06:09 -0400

    feat(core): add server exports and express peer dependencies

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 export { bloomAuth } from './auth';
 export type { BloomAuthConfig, BloomAuth, User, Session, BloomPlugin, BloomHandlerContext, GenericRequest, GenericResponse } from './types';
+export type { BloomServerConfig, BloomServerInstance } from './types/server';
 export * from './utils/crypto';
 export * from './models';
