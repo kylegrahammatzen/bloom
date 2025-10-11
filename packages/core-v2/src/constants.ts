7d17180c1b74d6c6a2a93cdccb2930120ed5424b
Kyle <kgrahammatzen@gmail.com>
2025-10-11 01:21:31 -0400

    feat(backend): add constants for token expiration times

---
+++ packages/core-v2/src/constants.ts
@@ -0,0 +1,17 @@
+/**
+ * Default session expiry duration in seconds
+ * @default 604800 (7 days)
+ */
+export const DEFAULT_SESSION_EXPIRY = 7 * 24 * 60 * 60
+
+/**
+ * Default minimum password length
+ * @default 8
+ */
+export const DEFAULT_MIN_PASSWORD_LENGTH = 8
+
+/**
+ * Default maximum password length
+ * @default 128
+ */
+export const DEFAULT_MAX_PASSWORD_LENGTH = 128
