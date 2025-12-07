028b81b8ff29719c1497328f5caac9ba97a9170a
Kyle <kgrahammatzen@gmail.com>
2025-12-07 10:30:59 -0500

    feat(backend): remove v1 demo apps

--- apps/react-router-v7/src/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, layout } from "@react-router/dev/routes";
-
-export default [
-  layout("./components/layout.tsx", [
-    index("./pages/home.tsx"),
-  ]),
-] satisfies RouteConfig;
